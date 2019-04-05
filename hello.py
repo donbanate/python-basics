f875e43e6b15b27e0439a4b6ec66e9c8725e6a3e
donbanate <lbanate97@gmail.com>
2019-04-06 00:17:13 +0800

    First Commit

---
+++ hello.py
@@ -0,0 +1,7 @@
+
+age = 22
+name = 'Lyndon'
+address = 'Barangay 56, Pasay City'
+
+print('{firstName} was {number} years old when he wrote this book he was living at {living}'.format(firstName=name, number=age, living=address)) #format({0}, {1}) array
+print('Why is {0} playing with that python?'.format(name))
